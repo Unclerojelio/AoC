1b052edfc2b85badc1f8a7d421a82edbc0824fba
Roger Banks <roger_banks@mac.com>
2023-12-06 09:59:21 -0600

    Update game.py

--- 2023/Day_01/game.py
+++ 2023/Day_01/game.py
@@ -1,12 +1,1 @@
-import sys
 
-lines = sys.stdin.readlines()
-lines = [line.rstrip() for line in lines]
-
-number = []
-for line in lines:
-    line = line[4:]
-    index = line.find(":")
-    number = line[:index]
-    line = line[index+2:]
-    print(int(number), line)
