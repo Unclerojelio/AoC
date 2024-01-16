6df55cc31a1c7637cf2104580244bb5d7314f870
Roger Banks <roger_banks@mac.com>
2024-01-15 19:31:38 -0600

    Day 2 Part 2 correct.

--- 2023/Day_02/game.py
+++ 2023/Day_02/game.py
@@ -8,9 +8,13 @@ lines = [line.rstrip() for line in lines]
 
 number = []
 total = 0
+power_sum = 0
 
 for line in lines:
     game_result = True
+    min_red = 0
+    min_green = 0
+    min_blue = 0
     games = []
     attempts = []
     line = line[4:]
@@ -32,6 +36,15 @@ for line in lines:
         green_result = True
         blue_result = True
         for pull in attempt:
+            if pull[1] == 'red':
+                if int(pull[0]) > min_red:
+                    min_red = int(pull[0])
+            if pull[1] == 'green':
+                if int(pull[0]) > min_green:
+                    min_green = int(pull[0])
+            if pull[1] == 'blue':
+                if int(pull[0]) > min_blue:
+                    min_blue = int(pull[0])
             if pull[1] == 'red' and int(pull[0]) > 12:
                 red_result = False
             if pull[1] == 'green' and int(pull[0]) > 13:
@@ -44,8 +57,10 @@ for line in lines:
     #print(f"Game result: {game_result}")
     if game_result:
         total += int(number)
+    power_sum += min_red * min_green * min_blue
 assert total == 2406
-print(f"Total: {total}") #2406 is correct
+assert power_sum == 78375
+print(f"Day 1 answer: {total} Day 2 answer: {power_sum}") #2406 is correct, 78375 is correct
 
 end = time.time()
 print("Elapsed time:", (end-start) * 10**3, "ms")
