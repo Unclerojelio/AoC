9ab5f9c85ec3adb7b5064760972146605756d253
Roger Banks <roger_banks@mac.com>
2024-01-18 12:33:47 -0600

    2023 Day 4 Part 2 correct.

--- 2023/Day_04/cards.py
+++ 2023/Day_04/cards.py
@@ -1,14 +1,13 @@
-import sys
 import time
 
 start = time.time()
 
-#lines = sys.stdin.readlines()
-#lines = [line.rstrip() for line in lines]
-
 lines = open(0).read().splitlines()
 sum = 0
+cards = []
+card_num = 0
 for line in lines:
+    card_num += 1
     index = line.find(":")
     line = line[index+1:]
     winners, choices = line.split("|")
@@ -20,8 +19,23 @@ for line in lines:
             count += 1
     if count >= 1:
         sum += 2**(count - 1)
+
+    cards.append([card_num, winners, choices, 1])
+
+ans2 = 0
+for card in cards:
+    for _ in range(card[3]):
+        count = 0
+        for choice in card[2]:
+            if choice in card[1]:
+                count += 1
+        for index in range(count):
+            cards[card[0] + index][3] += 1
+    ans2 += card[3]
+
 assert sum == 24160
-print(f"The answer for part 1: {sum}")
+assert ans2 == 5659035
+print(f"The answer for Part 1: {sum}, The answer for Part 2: {ans2}")
 
 end = time.time()
 print("Elapsed time:", (end-start) * 10**3, "ms")
