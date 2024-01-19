d8777354bcf6fe121a369f06e67511ea8bd68b68
Roger Banks <roger_banks@mac.com>
2024-01-19 14:11:54 -0600

    2023 Day 5 Part 1 correct.

--- 2023/Day_05/seeds.py
+++ 2023/Day_05/seeds.py
@@ -2,7 +2,26 @@ import time
 
 start = time.time()
 
-lines = open(0).read().splitlines()
+seeds, *blocks = open(0).read().split("\n\n")
+
+seeds = list(map(int,seeds.split(':')[1].split()))
+
+for block in blocks:
+    ranges = []
+    for line in block.splitlines()[1:]:
+        a, b, c  = map(int, line.split())
+        ranges.append((a, b, c))
+    new = []
+    for x in seeds:
+        for a, b, c in ranges:
+            if b <= x < b + c:
+                new.append(x - b + a)
+                break
+        else:
+            new.append(x)
+
+    seeds = new
+print(min(seeds))
 
 
 end = time.time()
