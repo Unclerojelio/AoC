92bd0e016bb775462a9fddd6e9e3b2bc80bc4af5
Roger Banks <roger_banks@mac.com>
2024-01-24 22:15:20 -0600

    2015 Day 5 Part 1 correct.

--- 2015/Day_06/lights.py
+++ 2015/Day_06/lights.py
@@ -3,11 +3,11 @@ import time
 start_time = time.time()
 lines = open(0).read().splitlines()
 
-xs = 10
-ys = 10
-lights = [[0]*xs]*ys
+xs = 1000
+ys = 1000
+lights = [[0] * xs for _ in range(ys)]
 
-lines = ['turn on 0,0 through 0,0']
+#lines = ['turn on 3,4 through 5,7', 'turn off 5,7 through 6,8', 'toggle 3,4 through 5,7']
 
 for line in lines:
     line = line.split(' ')
@@ -22,24 +22,19 @@ for line in lines:
     start_y = int(start_y)
     end_x = int(end_x)
     end_y = int(end_y)
-    # print(line)
-    #print(cmd, start_x, start_y, end_x, end_y)
 
     if cmd == 'on':
-        print(cmd, start_x, start_y, end_x, end_y)
-        for y in range(start_y, end_y+1):
-            for x in range(start_x, end_x+1):
+        for y in range(start_y, end_y + 1):
+            for x in range(start_x,end_x + 1):
                 lights[y][x] = 1
         
     elif cmd == 'off':
-        print(cmd, start_x, start_y, end_x, end_y)
         for y in range(start_y, end_y+1):
             for x in range(start_x, end_x+1):
                 lights[y][x] = 0
 
 
     elif cmd == 'toggle':
-        print(cmd, start_x, start_y, end_x, end_y)
         for y in range(start_y, end_y+1):
             for x in range(start_x, end_x+1):
                 if lights[y][x] == 0:
@@ -51,13 +46,12 @@ for line in lines:
 count = 0
 for y in range(ys):
     for x in range(xs):
-        #print(lights[y])
-        #count += sum(lights[y])
-        print(lights[y][x], end=' ')
+        #print(lights[y][x], end=' ')
         count += lights[y][x]
-    print()
+    #print()
 
-#692000 is too high
+#692000 is too high, 486027 is too low.
+assert count == 543903
 print(f"Lights: {count}")
 
 print("Elapsed time:", (time.time() - start_time) * 10**3, "ms")
