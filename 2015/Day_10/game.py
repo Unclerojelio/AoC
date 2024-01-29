47a242f60960af50c985dbca2b999b483f2d1b30
Roger Banks <roger_banks@mac.com>
2024-01-29 15:11:39 -0600

    2015 Day 10 Part 1 correct.

--- 2015/Day_10/game.py
+++ 2015/Day_10/game.py
@@ -1,9 +1,21 @@
 input = "1113122113"
 
-i = 0
-c = input[i]
-while i < len(input):
-    count = 0
-    if c == input[i]:
-        count += 1
-    i += 1
+def solve(input):
+    output = ""
+    while len(input) > 0:
+        i = 0
+        c = input[i]
+        count = 0
+        while i < len(input):
+            if c != input[i]:
+                break
+            count += 1
+            i += 1
+        input = input[i:]
+        output += str(count) + c
+    return output
+
+for _ in range(40):
+    output = solve(input)
+    print(len(output))
+    input = output
