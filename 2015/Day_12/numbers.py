30cf1b9d17efcb8677ad5f9d2413e6cb0b1bc48c
Roger Banks <roger_banks@mac.com>
2024-01-30 12:43:37 -0600

    2015 Day 12 amended.

--- 2015/Day_12/numbers.py
+++ 2015/Day_12/numbers.py
@@ -3,7 +3,6 @@ import re
 
 def getNumbers(line):
     numbers = re.findall(r'-?[0-9]+', line)
-    print(numbers)
     return numbers
 
 start_time = time.time()
@@ -17,7 +16,7 @@ for line in lines:
 # line = r'{"a":[-1,1]}'
 #line = r'{"e":[[{"e":86,"c":23,"a":{"a":[120,169,"green","red","orange"],"b":"red"},"g":"yellow","b":["yellow"],"d":"red","f":-19},{"e":-47,"a":[2],"d":{"a":"violet"},"c":"green","h":"orange",' 
 #ans1 = sum([int(i) for i in getNumbers(line)])
-assert ans1 = 111754
+assert ans1 == 111754
 print(ans1)
 
 print("Elapsed time:", (time.time() - start_time) * 10**3, "ms")
