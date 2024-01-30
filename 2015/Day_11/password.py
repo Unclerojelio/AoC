8880c963edf31281aa7a509d14e6ef0e3ed11f76
Roger Banks <roger_banks@mac.com>
2024-01-30 11:33:05 -0600

    2015 Day 11 Part 1 and Part 2 correct.

--- 2015/Day_11/password.py
+++ 2015/Day_11/password.py
@@ -1,21 +1,58 @@
 import time
+import string
+
+def inc_pass(input):
+    letters = string.ascii_lowercase
+    i = len(input) - 1
+
+    if input[i] == 'z':
+        while input[i] == 'z':
+            input = input[:i] + 'a' + input[i+1:] 
+            i -= 1
+        index = letters.index(input[i])
+        input = input[:i] + letters[index + 1] + input[i+1:]
+    else:
+        index = letters.index(input[-1])
+        input = input[:-1] + letters[index + 1]
+    return input
+
+def rule_1(input):
+    triples = ['abc', 'bcd', 'cde','def','efg', 'fgh', 'ghi','hij','ijk','jkl','klm','lmn','mno','nop','opq', 'pqr', 'qrs','rst','stu','tuv','uvw','vwx','wxy','xyz']
+    for triple in triples:
+        if triple in input:
+            return True
+    return False
+
+def rule_2(input):
+    if 'i' in input or 'o' in input or 'l' in input:
+        return False
+    return True
+
+def rule_3(input):
+    doubles = ['aa', 'bb','cc','dd','ee','ff','gg','hh','ii','jj','kk','ll','mm','nn','oo','pp','qq','rr','ss','tt','uu','vv','ww','xx','yy', 'zz']
+    finds = {}
+    for double in doubles:
+        index = input.find(double)
+        if index != -1:
+            finds[double] = index
+    if len(finds) >= 2:
+        return True
+    return False
 
 def solve(input):
-    ans1 = ''
-    ans2 = ''
-    i = len(input)
-    while i >= 0:
-        pass
-
-    return [ans1, ans2]
+    while True:
+        input = inc_pass(input)
+        if rule_2(input) and rule_1(input) and rule_3(input):
+            return input
 
 def main():
     start = time.time()
     input = "cqjxjnds"
 
-    ans1, ans2 = solve(input)
-    #assert ans1 == 1333
-    #assert ans2 == 2046
+    ans1 = solve(input)
+    ans2 = solve(ans1)
+    assert ans1 == 'cqjxxyzz'
+    assert ans2 == 'cqkaabcc'
     print("Answer 1:", ans1, "Answer 2: ", ans2)
 
     end = time.time()
