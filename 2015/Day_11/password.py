d0ddcb6447a0d1154b003b4abb3fb33baf8f18ab
Roger Banks <roger_banks@mac.com>
2024-01-29 20:58:28 -0600

    Add 2015 Day 11 files.

---
+++ 2015/Day_11/password.py
@@ -0,0 +1,26 @@
+import time
+
+def solve(input):
+    ans1 = ''
+    ans2 = ''
+    i = len(input)
+    while i >= 0:
+        pass
+
+    return [ans1, ans2]
+
+def main():
+    start = time.time()
+    input = "cqjxjnds"
+
+    ans1, ans2 = solve(input)
+    #assert ans1 == 1333
+    #assert ans2 == 2046
+    print("Answer 1:", ans1, "Answer 2: ", ans2)
+
+    end = time.time()
+    print("Elapsed time:", (end-start) * 10**3, "ms")
+
+
+if __name__=="__main__":
+    main()
