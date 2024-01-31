ccf6537285316fa7ee81f602b0c4a14994baec81
Roger Banks <roger_banks@mac.com>
2024-01-31 13:08:38 -0600

    2015 Day 12 Both parts correct.

--- 2015/Day_12/numbers.py
+++ 2015/Day_12/numbers.py
@@ -1,37 +1,88 @@
 import time
 import re
 
-start_time = time.time()
-json_doc = open(0).read()
-numbers = re.findall(r'-?[0-9]+', json_doc)
-ans1 = sum(map(int, numbers))
-assert ans1 == 111754
-print(ans1)
+def sum_nums(line):
+    numbers = re.findall(r'-?[0-9]+', line)
+    return sum(map(int, numbers))
 
+def is_balanced(line):
+    stack = []
+    for ch in line:
+        if ch == '[' or ch == '{':
+            stack.append(ch)
+        elif ch == ']' or ch == '}':
+            if len(stack) == 0:
+                return False
+            elif (stack[-1] == '{' and ch == '}') or (stack[-1] == '[' and ch == ']'):
+                stack.pop()
+    if len(stack) == 0:
+        return True
+    else:
+        return False
 
-#line = r'{"a":2"b":4}'
-#line = r'[[[3]]]'
-#line = r'{"a":{"b":4},"c":-1}'
-#line = r'{"a":[-1,1]}'
-#line = r'{"e":[[{"e":86,"c":23,"a":{"a":[120,169,"green","red","orange"],"b":"red"},"g":"yellow","b":["yellow"],"d":"red","f":-19},{"e":-47,"a":[2],"d":{"a":"violet"},"c":"green","h":"orange",}}]]]}' 
-
-#json_doc = line
-stack = []
-for ch in json_doc:
-    print(stack, ch)
-    if ch == '[' or ch == '{':
-        stack.append(ch)
-    elif ch == ']' or ch == '}':
-        if len(stack) == 0:
-            print("False")
-        elif (stack[-1] == '{' and ch == '}') or (stack[-1] == '[' and ch == ']'):
+def parse_line(line):
+    stack = []
+    for ch in line:
+        temp = ''
+        if ch == ']':
+            while stack[-1] != '[':
+                temp = stack.pop() + temp
             stack.pop()
-if len(stack) == 0:
-    print("True")
-else:
-    print("False")
+            stack.append(str(sum_nums(temp)))
+        elif ch == '}':
+            while stack[-1] != '{':
+                temp = stack.pop() + temp
+            stack.pop()
+            if temp.find('red') == -1:
+                stack.append(str(sum_nums(temp)))
+        else:
+            stack.append(ch)
+    return stack
     
+def do_tests():
+    assert sum_nums(r'[1,2,3]') == 6
+    assert sum_nums(r'{"a":2"b":4}') == 6
+    assert sum_nums(r'[[[3]]]') == 3
+    assert sum_nums(r'{"a":{"b":4},"c":-1}') == 3
+    assert sum_nums(r'{"a":[-1,1]}') == 0
+    assert sum_nums(r'[-1,{"a":1}]') == 0
+    assert sum_nums(r'[-1,{"a":2}]') != 0
+
+    assert is_balanced(r'[1,2,3]') == True
+    assert is_balanced(r'{"a":2"b":4}') == True
+    assert is_balanced(r'[[[3]]]') == True
+    assert is_balanced(r'{"a":{"b":4},"c":-1}') == True
+    assert is_balanced(r'{"a":[-1,1]}') == True
+    assert is_balanced(r'[-1,{"a":1}]') == True
+    assert is_balanced(r'[-1,{"a":1}}') == False
     
+    assert sum(map(int, parse_line(r'[1,2,3]'))) == 6
+    assert sum(map(int, parse_line(r'{"a":2"b":4}'))) == 6
+    assert sum(map(int, parse_line(r'[[[3]]]'))) == 3
+    assert sum(map(int, parse_line(r'{"a":{"b":4},"c":-1}'))) == 3
+    assert sum(map(int, parse_line(r'{"a":[-1,1]}'))) == 0
+    assert sum(map(int, parse_line(r'[-1,{"a":1}]'))) == 0
+    assert sum(map(int, parse_line(r'[-1,{"a":2}]'))) != 0
+    assert sum(map(int, parse_line(r'[1,{"c":"red","b":2},3]'))) == 4
+    assert sum(map(int, parse_line(r'{"d":"red","e":[1,2,3,4],"f":5}'))) == 0
+    assert sum(map(int, parse_line(r'[1,"red",5]'))) == 6
 
+    return True
 
-print("Elapsed time:", (time.time() - start_time) * 10**3, "ms")
+def main():
+    start_time = time.time()
+
+    if do_tests():
+        print("Tests Passed")
+
+    json_doc = open(0).read()
+    ans1 = sum_nums(json_doc)
+    ans2 = sum(map(int, parse_line(json_doc)))
+    assert ans1 == 111754
+    assert ans2 == 65402
+    print("Answer 1:", ans1, "Answer 2: ", ans2)
+
+    print("Elapsed time:", (time.time() - start_time) * 10**3, "ms")
+
+if __name__=="__main__":
+    main()
