b8781215b42598cd9a78eda585d897e9e5a28c96
Roger Banks <roger_banks@mac.com>
2024-01-30 22:23:06 -0600

    2015 Day 12 Fix bracket checker.

--- 2015/Day_12/numbers.py
+++ 2015/Day_12/numbers.py
@@ -1,34 +1,30 @@
 import time
 import re
 
-def getNumbers(line):
-    numbers = re.findall(r'-?[0-9]+', line)
-    return numbers
-
 start_time = time.time()
-ans1 = 0
 json_doc = open(0).read()
-lines = json_doc.split(',')
-for line in lines:
-    ans1 += sum([int(i) for i in getNumbers(line)])
-# line = r'{"a":2"b":4}'
-# line = r'[[[3]]]'
-# line = r'{"a":{"b":4},"c":-1}'
-# line = r'{"a":[-1,1]}'
-#line = r'{"e":[[{"e":86,"c":23,"a":{"a":[120,169,"green","red","orange"],"b":"red"},"g":"yellow","b":["yellow"],"d":"red","f":-19},{"e":-47,"a":[2],"d":{"a":"violet"},"c":"green","h":"orange",' 
-#ans1 = sum([int(i) for i in getNumbers(line)])
+numbers = re.findall(r'-?[0-9]+', json_doc)
+ans1 = sum(map(int, numbers))
 assert ans1 == 111754
 print(ans1)
 
+
+#line = r'{"a":2"b":4}'
+#line = r'[[[3]]]'
+#line = r'{"a":{"b":4},"c":-1}'
+#line = r'{"a":[-1,1]}'
+#line = r'{"e":[[{"e":86,"c":23,"a":{"a":[120,169,"green","red","orange"],"b":"red"},"g":"yellow","b":["yellow"],"d":"red","f":-19},{"e":-47,"a":[2],"d":{"a":"violet"},"c":"green","h":"orange",}}]]]}' 
+
+#json_doc = line
 stack = []
 for ch in json_doc:
-    print(stack)
+    print(stack, ch)
     if ch == '[' or ch == '{':
         stack.append(ch)
     elif ch == ']' or ch == '}':
         if len(stack) == 0:
             print("False")
-        elif ch == stack[-1]:
+        elif (stack[-1] == '{' and ch == '}') or (stack[-1] == '[' and ch == ']'):
             stack.pop()
 if len(stack) == 0:
     print("True")
