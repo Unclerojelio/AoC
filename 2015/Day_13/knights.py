ad1d7b9859c013a8b3d4a7e376410be31965da1b
Roger Banks <roger_banks@mac.com>
2024-01-31 21:07:16 -0600

    2015 Day 13 Factor out solve() function and add test data.

--- 2015/Day_13/knights.py
+++ 2015/Day_13/knights.py
@@ -3,12 +3,24 @@ import re
 from itertools import permutations
 
 def do_tests():
+    lines = '''Alice would gain 54 happiness units by sitting next to Bob.
+Alice would lose 79 happiness units by sitting next to Carol.
+Alice would lose 2 happiness units by sitting next to David.
+Bob would gain 83 happiness units by sitting next to Alice.
+Bob would lose 7 happiness units by sitting next to Carol.
+Bob would lose 63 happiness units by sitting next to David.
+Carol would lose 62 happiness units by sitting next to Alice.
+Carol would gain 60 happiness units by sitting next to Bob.
+Carol would gain 55 happiness units by sitting next to David.
+David would gain 46 happiness units by sitting next to Alice.
+David would lose 7 happiness units by sitting next to Bob.
+David would gain 41 happiness units by sitting next to Carol.'''
+
+    lines = lines.splitlines()
+    assert solve(lines) == 330
     return True
 
-def main():
-    start_time = time.time()
-
-    lines = open(0).read().splitlines()
+def solve(lines):
     pairs = {}
     guests = set()
     for line in lines:
@@ -29,13 +41,20 @@ def main():
             happiness += pairs[position[i], position[(i+1) % len(position)]]
             happiness += pairs[position[i], position[(i-1) % len(position)]]
         ans1 = max(ans1, happiness)
-    print(ans1)
 
+    return ans1
+
+def main():
+    start_time = time.time()
 
     if do_tests():
         print("Tests Passed")
+
+    lines = open(0).read().splitlines()
+    ans1 = solve(lines)
     
-    #assert ans1 == 111754
+    assert ans1 == 709
+    ans2 = 0
     #assert ans2 == 65402
     print("Answer 1:", ans1, "Answer 2: ", ans2)
 
