14e5abe87fa57f55b7e045815aa9654d6b3ea694
Roger Banks <roger_banks@mac.com>
2024-02-03 20:50:02 -0600

    Part 1 correct.

--- 2015/Day_16/aunt.py
+++ 2015/Day_16/aunt.py
@@ -10,8 +10,24 @@ def parse_file(lines):
         aunts.append({'Number': aunt[1][:-1], aunt[2][:-1]: aunt[3][:-1], aunt[4][:-1]: aunt[5][:-1], aunt[6][:-1]: aunt[7]})
     return aunts
 
-def solve(lines):
-    ans1 = 0
+def solve(aunts):
+    aunt_sue = {'children': '3', 
+                'cats': '7',
+                'samoyeds': '2', 
+                'pomeranians': '3', 
+                'akitas': '0', 
+                'vizslas': '0', 
+                'goldfish': '5', 
+                'trees': '3', 
+                'cars': '2', 
+                'perfumes': '1'}
+    for aunt in aunts:
+        count = 0
+        for key in aunt_sue.keys():
+            if key in aunt.keys() and aunt_sue[key] == aunt[key]:
+                count += 1
+            if count == 3:
+                ans1 = int(aunt['Number'])
     ans2 = 0
     return ans1, ans2
 
@@ -20,7 +36,7 @@ def main():
     aunts = parse_file(open(0).read().splitlines())
     ans1, ans2 = solve(aunts)
     
-    # assert ans1 == 18965440
+    assert ans1 == 103
     # assert ans2 == 15862900
     print("Answer 1:", ans1, "Answer 2: ", ans2)
     print("Elapsed time:", (time.time() - start_time) * 10**3, "ms")
