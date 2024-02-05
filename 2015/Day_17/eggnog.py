64a19de93e14565983ee7279eceae58aa5cbc3b9
Roger Banks <roger_banks@mac.com>
2024-02-04 19:41:13 -0600

    Day 17 Part 1 correct.

--- 2015/Day_17/eggnog.py
+++ 2015/Day_17/eggnog.py
@@ -1,6 +1,6 @@
 import time
 import re
-from itertools import permutations
+from itertools import combinations
 
 
 def parse_file(lines):
@@ -9,6 +9,10 @@ def parse_file(lines):
 
 def solve(containers, amount):
     ans1 = 0
+    for r in range(1, len(containers) +1):
+        for z in combinations(containers, r):
+            ans1 += sum(z) == amount
+
     ans2 = 0
     return ans1, ans2
 
@@ -20,10 +24,10 @@ def do_tests():
     5'''
     lines = lines.splitlines()
     containers = parse_file(lines)
-    ans1 = solve(containers, 25)
+    ans1, ans2 = solve(containers, 25)
     #ans2 = solve(ingredients, partitions, True)
 
-    # assert ans1 == 62842880
+    assert ans1 == 4
     # assert ans2 == 57600000
     return True
 
@@ -36,7 +40,7 @@ def main():
     containers = parse_file(open(0).read().splitlines())
     ans1, ans2 = solve(containers, 150)
     
-    # assert ans1 == 103
+    assert ans1 == 654
     # assert ans2 == 405
     print("Answer 1:", ans1, "Answer 2: ", ans2)
     print("Elapsed time:", (time.time() - start_time) * 10**3, "ms")
