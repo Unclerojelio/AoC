d8a74ed71c74ecd331f3468a3be6e27ac41f0314
Roger Banks <roger_banks@mac.com>
2024-02-08 14:28:14 -0600

    Part 2 correct.

--- 2015/Day_20/presents.py
+++ 2015/Day_20/presents.py
@@ -3,35 +3,55 @@ import re
 import itertools
 import math
 
-def divSum(n) :
+def divisors(n) :
+    result = []
     if(n == 1):
-       return 1
+        result.append(1)
+        return result
 
-    result = 0
     for i in range(2,(int)(math.sqrt(n))+1) :
         if (n % i == 0) :
             if (i == (n/i)) :
-                result = result + i
+                result.append(i)
             else :
-                result = result + (i + n//i)
+                result.extend([i, n//i])
+    result.extend([1, n])
+    return result
 
-    return (result + n + 1)
+def sumdiv(n) :
+    result = 0
+    for i in range(2,(int)(math.sqrt(n))) :
+        if (n % i == 0) :
+            if (i == (n/i)) :
+                if 50 * i >= n:
+                    result = result + i
+            else :
+                if 50 * i >= n:
+                    result = result + i
+                if 50 * (n/i) >= n:
+                    result = result + (n / i)
+    return int(result + n)
 
 def solve(goal):
     for i in itertools.count():
-        presents = 10*divSum(i)
+        presents = 10 * sum(divisors(i))
         if presents >= goal:
             return i
 
+def solve2(goal):
+    for i in itertools.count():
+        presents = 11 * sumdiv(i)
+        if presents >= goal:
+            return i
 
 def main():
     start_time = time.time()
-
-    ans1 = solve(29000000)
-    ans2 = 0
+    ans1 = 0
+    #ans1 = solve(29000000)
+    ans2 = solve2(29000000)
     
-    assert ans1 == 665280
-    # assert ans2 == 924
+    #assert ans1 == 665280
+    assert ans2 == 705600
     print("Answer 1:", ans1, "Answer 2: ", ans2)
     print("Elapsed time:", (time.time() - start_time) * 10**3, "ms")
 
