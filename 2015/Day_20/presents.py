d5f4680e42c387d98a5ba71d191c414a71998f0f
Roger Banks <roger_banks@mac.com>
2024-02-08 10:46:18 -0600

    Part 1 correct.

--- 2015/Day_20/presents.py
+++ 2015/Day_20/presents.py
@@ -1,28 +1,36 @@
 import time
 import re
-from itertools import combinations
+import itertools
+import math
 
-def solve():
+def divSum(n) :
+    if(n == 1):
+       return 1
 
-    house = 1
-    presents = 0
-    elf = 1
-    while presents <= 29000000:
-        presents += elf * 10
-        house += 1
-        elf += 1
+    result = 0
+    for i in range(2,(int)(math.sqrt(n))+1) :
+        if (n % i == 0) :
+            if (i == (n/i)) :
+                result = result + i
+            else :
+                result = result + (i + n//i)
 
-    print(house, elf, presents)
-    return house
+    return (result + n + 1)
+
+def solve(goal):
+    for i in itertools.count():
+        presents = 10*divSum(i)
+        if presents >= goal:
+            return i
+
 
 def main():
     start_time = time.time()
-    
 
-    ans1 = solve()
+    ans1 = solve(29000000)
     ans2 = 0
     
-    # assert ans1 == 814
+    assert ans1 == 665280
     # assert ans2 == 924
     print("Answer 1:", ans1, "Answer 2: ", ans2)
     print("Elapsed time:", (time.time() - start_time) * 10**3, "ms")
