19c9c0c876d5f53d43b2b1578a38c2e52ea95485
Roger Banks <roger_banks@mac.com>
2024-02-08 20:51:43 -0600

    Part 1 correct.

--- 2015/Day_21/rpg.py
+++ 2015/Day_21/rpg.py
@@ -56,8 +56,6 @@ def find_unique_combinations(weapons, armors, rings):
         for armor in [None] + armor_list:
             for ring in [None] + ring_list + [ring_list[i:i+2] for i in range(len(ring_list))]:
                 combinations.append((weapon, armor, ring))
-    #print(combinations)
-    #unique_combinations = set(combinations)
     return combinations
 
 def fight(player, boss):
@@ -99,18 +97,40 @@ def main():
               'damage' : 8,
               'armor' : 2}
 
-    ans1 = fight(player, boss)
-    #ans2 = solve()
-
-    # Find unique combinations
+    wins = set()
     unique_combinations = find_unique_combinations(weapons, armors, rings)
-
-    # Print the unique combinations
     for combination in unique_combinations:
-        print(combination)
+        cost = 0
+        damage = 0
+        armor = 0
+        for item in combination:
+            if isinstance(item, list):
+                for ring in item:
+                    cost += rings[ring][0]
+                    damage += rings[ring][1]
+                    armor += rings[ring][2]
+            elif item in weapons:
+                cost += weapons[item][0]
+                damage += weapons[item][1]
+                armor += weapons[item][2]
+            elif item in armors:
+                cost += armors[item][0]
+                damage += armors[item][1]
+                armor += armors[item][2]
+            elif item in rings:
+                cost += rings[item][0]
+                damage += rings[item][1]
+                armor += rings[item][2]
+        player['health'] = 100
+        player['damage'] = damage
+        player['armor'] = armor
+        boss['health'] = 100
+        if fight(player, boss):
+            wins.add(cost)
 
+    ans1 = min(wins)
     ans2 = 0
-    # assert ans1 == 518
+    assert ans1 == 91
     # assert ans2 == 0 
     print("Answer 1:", ans1, "Answer 2: ", ans2)
     print("Elapsed time:", (time.time() - start_time) * 10**3, "ms")
