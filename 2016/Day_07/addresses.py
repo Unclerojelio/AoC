bcb830c762e73e8b35a60ab21119d44ca4081e29
Roger Banks <roger_banks@mac.com>
2024-02-19 21:30:44 -0600

    Part 2 correct.

--- 2016/Day_07/addresses.py
+++ 2016/Day_07/addresses.py
@@ -17,6 +17,17 @@ def has_special_pattern(substrings):
                 return True
     return False
 
+def has_special_pattern2(substrings):
+    area_broadcast_accessors = []
+    for substring in substrings:
+        for i in range(len(substring) - 2):
+            if substring[i] != substring[i + 1] and substring[i] == substring[i + 2]:
+                area_broadcast_accessors.append(substring[i:i+3])
+    return area_broadcast_accessors
+
+def bab(aba):
+    return aba[1:-1]+aba[:1]+aba[1:-1]
+
 def do_tests():
     lines = '''abba[mnop]qrst
     abcd[bddb]xyyx
@@ -42,6 +53,14 @@ def solve(addresses):
         substrings_inside_brackets, substrings_outside_brackets = extract_substrings(address)
         if not has_special_pattern(substrings_inside_brackets) and has_special_pattern(substrings_outside_brackets):
             ans1 += 1
+        area_broadcast_accessors = has_special_pattern2(substrings_outside_brackets)
+        SSL = False
+        for aba in area_broadcast_accessors:
+            for sequence in substrings_inside_brackets:
+                if bab(aba) in sequence:
+                    SSL = True
+        if SSL:
+            ans2 += 1
         
     return ans1, ans2
 
@@ -56,7 +75,7 @@ def main():
     ans1, ans2 = solve(addresses)
     
     assert ans1 == 115
-    # assert ans2 == "aovueakv"
+    assert ans2 == 231
     print("Answer 1:", ans1, "Answer 2: ", ans2)
 
     print("Elapsed time:", (time.time() - start_time) * 10**3, "ms")
