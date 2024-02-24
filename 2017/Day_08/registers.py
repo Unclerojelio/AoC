0f05ba4dfd8a2cc30a2d4a97c510ef5bc24947cc
Roger Banks <roger_banks@mac.com>
2024-02-23 18:37:50 -0600

    Both parts correct.

--- 2017/Day_08/registers.py
+++ 2017/Day_08/registers.py
@@ -1,24 +1,34 @@
 import time
+from collections import defaultdict
 
 def do_tests():
-    line = '''b inc 5 if a > 1
+    lines = '''b inc 5 if a > 1
 a inc 1 if b < 5
 c dec -10 if a >= 1
 c inc -20 if c == 10'''
     lines = lines.splitlines()
-  for line in lines:
-    print(line)
-    
-    ans1, ans2 = solve()
+    ans1, ans2 = solve(lines)
 
-    # assert ans1 == 5
-    # assert ans2 == 4
+    assert ans1 == 1
+    assert ans2 == 10
     return True
 
-def solve(blocks):
-  ans1 = 0
-  ans2 = 0
-  return ans1, ans2
+def solve(lines):
+    ans2 = 0
+    registers = defaultdict(int)
+    for line in lines:
+        tokens = line.split()
+        expr = str(registers[tokens[4]]) + tokens[5] + tokens[6]
+        if eval(expr):
+            if tokens[1] == 'inc':
+                registers[tokens[0]] += int(tokens[2])
+            else:
+                registers[tokens[0]] -= int(tokens[2])
+            ans2 = max(ans2, registers[tokens[0]])
+
+    ans1 = max(registers.values())
+
+    return ans1, ans2
 
 def main():
     start_time = time.time()
@@ -26,10 +36,11 @@ def main():
     if do_tests():
         print("Tests Passed")
 
-    ans1, ans2 = solve()
+    lines = open(0).read().splitlines()
+    ans1, ans2 = solve(lines)
     
-    # assert ans1 == 12841
-    # assert ans2 == 8038
+    assert ans1 == 8022
+    assert ans2 == 9819
     print("Answer 1:", ans1, "Answer 2: ", ans2)
 
     print("Elapsed time:", (time.time() - start_time) * 10**3, "ms")
