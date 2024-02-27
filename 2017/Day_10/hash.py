ba1077df9dfca5f29b5119f69241b18a7e8fb3f8
Roger Banks <roger_banks@mac.com>
2024-02-26 21:25:11 -0600

    Work in progress.

--- 2017/Day_10/hash.py
+++ 2017/Day_10/hash.py
@@ -9,19 +9,25 @@ def do_tests():
     return True
 
 def solve(line, num_elements):
-    elements = []
     skip = 0
-    for i in range(num_elements):
-        elements.append(i)
-
     position = 0
+    elements = list(range(num_elements))
+    print(elements)
     for length in line:
+        if position + length  < num_elements:
+            substr = elements[position:position + length]
+            substr = substr[::-1]
+            elements = elements[:position] + substr + elements[position + length:]
+            print(substr, elements)
+        else:
+            substr = elements[position:] + elements[:(position + length) % num_elements]
+            substr = substr[::-1]
+            print(substr[length - num_elements - 1:], elements[position - 1: (position + length) % num_elements + 1], substr[:length - num_elements - 1], position, length)
+            elements = substr[length - num_elements - 1:] + elements[position - 1: (position + length) % num_elements + 1] + substr[:length - num_elements - 1]
+            print(substr, elements)
         position = (position + length + skip) % num_elements
         skip += 1
-        print(length, position, skip)
-
-    print(line)
-    print(elements)
+        #print(length, position, skip)
 
     ans1 = elements[0] * elements[1]
     ans2 = 0
@@ -36,7 +42,7 @@ def main():
 
     line = [int(x) for x in open(0).read().strip().split(',')]
     num_elements = 256
-    ans1, ans2 = solve(line, num_elements)
+    #ans1, ans2 = solve(line, num_elements)
     
     #assert ans1 == 16869
     #assert ans2 == 7284
